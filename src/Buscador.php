7cd8ae7bc984d02e2d7246e101ec3428c35e3939
William Figueiredo <figueiredowill@gmail.com>
2021-06-02 15:58:39 -0300

    Meu primeiro projeto composer

---
+++ src/Buscador.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Alura\BuscadorDeCursos;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
+
+    /**
+     * @var Crawler
+     */
+    private $crawler;
+
+    /**
+     * Buscador constructor.
+     * @param ClientInterface $httpClient
+     * @param Crawler $crawler
+     */
+    public function __construct(
+        ClientInterface $httpClient,
+        Crawler $crawler
+    ) {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    /**
+     * @param string $url
+     * @return array
+     */
+    public function buscar(string $url)
+    {
+        $resposta = $this->httpClient->request('GET', $url);
+
+        $html = $resposta->getBody();
+        $this->crawler->addHtmlContent($html);
+
+        $elementosCursos = $this->crawler->filter('span.card-curso__nome');
+        $cursos = [];
+
+        foreach ($elementosCursos as $elemento) {
+            $cursos[] = $elemento->textContent;
+        }
+        return $cursos;
+    }
+}
